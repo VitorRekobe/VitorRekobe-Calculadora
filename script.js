341303b2365b6d4135d91848224725e07d79484d
VitorRekobe <98287250+VitorRekobe@users.noreply.github.com>
2022-10-01 17:33:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+var expressao = '';
+var numero = [];
+var operacao = [];
+var numTemp = '';
+var resultado = '';
+
+function printa(dado) {
+
+    if (dado == ' + ' || dado == ' - ' || dado == ' * ' || dado == ' / ' || dado == ' = ') {
+        operacao.push(dado);
+        numero.push(Number(numTemp));
+        numTemp = '';
+    } else {
+        numTemp += dado;
+    }
+
+    if (resultado == '') {
+        expressao += dado;
+        document.getElementById("formula").innerText = expressao;
+    }
+
+}
+
+function calcula() {
+    printa(' = ');
+    let qtdNum = numero.length;
+
+    if (resultado == '') {
+        for (let x = 0; x < qtdNum; x++) {
+            switch (operacao[x]) {
+
+                case ' + ':
+                    resultado = numero[x] + numero[x + 1];
+                    break;
+
+                case ' * ':
+                    resultado = numero[x] * numero[x + 1];
+                    break;
+
+                case ' / ':
+                    resultado = numero[x] / numero[x + 1];
+                    break;
+
+                case ' - ':
+                    resultado = numero[x] - numero[x + 1];
+                    break;
+            }
+            numero[x + 1] = resultado;
+        }
+        document.getElementById("resposta").innerText = resultado;
+    }
+}
+
+function apaga() {
+    expressao = "";
+    numero = [];
+    operacao = [];
+    numTemp = '';
+    resultado = '';
+    document.getElementById("formula").innerHTML = expressao;
+    document.getElementById("resposta").innerHTML = expressao;
+}
+
